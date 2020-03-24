f59613950db69404cb8e5900b5c48d59c2b20bb9
Jason Strouphauer <jdstroup10@gmail.com>
2020-03-23 19:11:18 -0500

    Updating html and js code

---
+++ Questions.js
@@ -0,0 +1,12 @@
+var questions = [
+	{
+		title: "Which of these is not an animal?",
+		choices: ["cat", "dog", "bird", "tree"],
+		answer: "tree"
+	},
+	{
+		title: "Which of these is a State?",
+		choices: ["Guam", "Kansas", "American Samoa", "Puerto Rico"],
+		answer: "Kansas"
+	}
+]
---
+++ Quiz.js
@@ -0,0 +1,9 @@
+var playButton = document.querySelector("#play");
+
+function startTimer() {
+    console.log("hello");
+    
+    //setTime();
+}
+
+playButton.addEventListener("click", startTimer);
